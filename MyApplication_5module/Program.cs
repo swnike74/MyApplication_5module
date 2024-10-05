ca3b96e57ccaabe666ac442d2d373848c1291501
Ведерников Сергей <swnike@mail.ru>
2024-10-05 19:39:55 +0500

    Добавьте файлы проекта.

---
+++ MyApplication_5module/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyApplication_5module
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
