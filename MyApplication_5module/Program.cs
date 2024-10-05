8f1b713db9cde90f2cb3fecbdfcd7bdc0a9a0a02
Ведерников Сергей <swnike@mail.ru>
2024-10-05 19:48:33 +0500

    Задание 1

--- MyApplication_5module/Program.cs
+++ MyApplication_5module/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            (string Name, string[] Dishes) User;
+
+
+            Console.WriteLine("Введите имя пользователя:");
+            User.Name = Console.ReadLine();
+
+            User.Dishes = new string[5];
+            for (int i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.WriteLine("Введите любимое блюдо номер {0}",i + 1);
+                User.Dishes[i] = Console.ReadLine();
+            }
+            Console.ReadKey();
         }
     }
 }
