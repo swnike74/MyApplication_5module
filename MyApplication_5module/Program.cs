8231f5af9d80b75a8f20e6926769e1258a8d1b9e
Ведерников Сергей <swnike@mail.ru>
2024-10-05 21:30:55 +0500

    Задание 5.1.6 - ввд массива с КВ и сортировка

--- MyApplication_5module/Program.cs
+++ MyApplication_5module/Program.cs
@@ -38,22 +38,41 @@
             return color;
         }
 
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            int temp = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if(result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }    
+                }
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            var favcolors = new string[3];
-
-            for (int i = 0; i < favcolors.Length; i++)
-            {
-                favcolors[i] = ShowColor();
-            }
-
-            Console.WriteLine("Мои любимые цвета:");
-            foreach(var fav in favcolors)
-            { 
-                Console.WriteLine(fav);
-            }
-
-            Console.ReadKey();
+            GetArrayFromConsole();
         }
     }
 }
