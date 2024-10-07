82751e6e8b72f200a67638d0d8e5b6125bea3254
Ведерников Сергей <swnike@mail.ru>
2024-10-07 11:50:46 +0500

    Задание 5.2.2 - ShowColor(name)

--- MyApplication_5module/Program.cs
+++ MyApplication_5module/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace MyApplication_5module
-{
+{//Задание 5.2.2
     internal class Program
     {
-        static string ShowColor()
+        static string ShowColor(string username)
         {
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
             var color = Console.ReadLine();
@@ -72,7 +72,31 @@
 
         static void Main(string[] args)
         {
-            GetArrayFromConsole();
+            var (name, age) = ("Евгения", 27);
+
+            Console.WriteLine("Моё имя: {0}", name);
+            Console.WriteLine("Мой возраст: {0}", age);
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами:");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+
+            var favcolors = new string[3];
+            for(int i = 0;i < favcolors.Length;i++)
+            {
+                favcolors[i] = ShowColor(name);
+            }
+
+            Console.WriteLine("Ваши любимые цвета;");
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
+            Console.ReadKey();
         }
     }
 }
